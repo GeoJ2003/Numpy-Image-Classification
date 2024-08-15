4f1477b0c89864edffb44e95bf0dabe2747d60fa
GeoJ2003 <georgeleejonesiii@gmail.com>
2024-08-14 22:26:51 -0700

    Real tests, but will most likely change later

--- test.py
+++ test.py
@@ -1,60 +1,22 @@
-import numpy as np
-import imageutils as img
-import classification as cls
-import matrixclass as ms
+import matrixclass as mc
 
-# Only works with greyscale images (2D arrays)
-def print_non_zero_values(array):
-    for row in array:
-            for value in row:
-                if value != 0:
-                    print(value, end=' ')
-                else:
-                    print('.', end=' ')
-            print()  # Newline after each row
+def test1():
+    matrix_classes = mc.MC_list('imgs/mnist', 2, ['zero', 'two', 'three', 'four', 'five', 'six', 'eight', 'nine'])
+    matrix_classes.classify_dirs('imgs/mnist', 1)
 
-# img_array = img.load_image_and_resize('imgs/dogs/Dog1.png', 100, 100)
+def test2():
+    matrix_classes = mc.MC_list('imgs/mnist', 2, ['seven'])
+    matrix_classes.classify_dirs('imgs/mnist', 3)
 
-# print_non_zero_values(img_array)
+def test3():
+    matrix_classes = mc.MC_list('imgs/mnist', 2, ['one'])
+    matrix_classes.classify_dirs('imgs/mnist', 3)
 
-# img_array = img.load_image_and_resize('imgs/dogs/Dog1.png', 100, 100)
+def test4():
+    matrix_classes = mc.MC_list('imgs/mnist', 2, ['two'])
+    matrix_classes.classify_dirs('imgs/mnist', 3)
 
-# img.save_image(img_pil, 'png', 'Dog1_resized')
-
-# img_pil.show()
-
-# dir = 'imgs/dogs'
-
-# imgsDict = img.load_images(dir)
-
-# print(imgsDict)
-
-# imgs_dict = img.load_images('imgs/mnist/seven')
-
-# cos_sim_table = cls.find_image_cosine_similarities(imgs_dict)
-
-# print(cos_sim_table)
-
-# cls.serve_similarity_table(imgs_dict, cos_sim_table)
-
-# seven_subspace = mc.MatrixClass('imgs/mnist/seven')
-
-# cls.project(seven_subspace, 'imgs/mnist/seven/101.jpg')
-
-# Step 1: Create a MatrixClass object
-one = ms.MatrixClass('imgs/mnist/one', 'One')
-seven = ms.MatrixClass('imgs/mnist/seven', 'Seven')
-
-one.proj_img_onto_subspace('imgs/mnist/seven/10179.jpg')
-seven.proj_img_onto_subspace('imgs/mnist/seven/10179.jpg')
-
-print()
-
-one.proj_img_onto_subspace('imgs/mnist/one/1002.jpg')
-seven.proj_img_onto_subspace('imgs/mnist/one/1002.jpg')
-
-# print("This is U:", seven_subspace.get_U())
-# print("This is S:", seven_subspace.get_S())
-# print("This is Vt:", seven_subspace.get_Vt())
-# cls.serve_similarity_table(seven_subspace.imgs_dict, seven_subspace.similarities)
+test1()
+# test2()
+# test3()
 
